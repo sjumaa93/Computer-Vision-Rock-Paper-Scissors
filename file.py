11d7f56eac49ff5997705ee92182340f9a939750
sjumaa93 <sjumaa93@gmail.com>
2022-03-07 19:46:42 +0000

    init

---
+++ file.py
@@ -0,0 +1,50 @@
+import cv2
+from keras.models import load_model
+import numpy as np
+model = load_model('keras_model.h5')
+cap = cv2.VideoCapture(0)
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+while True: 
+    ret, frame = cap.read()
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+    data[0] = normalized_image
+    prediction = model.predict(data)
+    cv2.imshow('frame', frame)
+    # Press q to close the window
+
+
+    import random
+
+    #if(prediction[0][3]) > 0.8:
+    #    user_choice = ('None')
+    #elif(prediction[0][1]) > 0.8:
+    #    user_choice = ('Paper')
+    #elif(prediction[0][2]) > 0.8:
+    #    user_choice = ('Scissors')
+    #else:
+    #    user_choice = ('Rock')
+    
+    #print(f'You chose {user_choice}')
+
+    computer_choice = random.randint(0,2)
+
+    if(computer_choice) == 0:
+        computer_choice = ('Rock')
+    elif(computer_choice) == 1:
+        computer_choice = ('Paper')
+    else:
+        computer_choice = ('Scissors')
+    
+    print(f'Computer chose {computer_choice}')
+    
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+            
+# After the loop release the cap object
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
