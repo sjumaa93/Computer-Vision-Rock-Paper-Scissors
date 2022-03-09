0ce7037a20c5547ae924b9198d79267c718536b0
sjumaa93 <sjumaa93@gmail.com>
2022-03-09 18:55:59 +0000

    added text

--- file.py
+++ file.py
@@ -1,6 +1,7 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import random
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -12,50 +13,42 @@ while True:
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
     data[0] = normalized_image
     prediction = model.predict(data)
-    cv2.imshow('frame', frame)
+
     # Press q to close the window
 
-    input('Press Enter to Start')
-    import random
-
-    if(prediction[0][3]) > 0.8:
+    if(prediction[0][3]) > 0.5:
         user_choice = ('None')
-    elif(prediction[0][1]) > 0.8:
+    elif(prediction[0][1]) > 0.5:
         user_choice = ('Paper')
-    elif(prediction[0][2]) > 0.8:
+    elif(prediction[0][2]) > 0.5:
         user_choice = ('Scissors')
     else:
         user_choice = ('Rock')
-    
     print(f'You chose {user_choice}')
 
     computer_choice = random.choice(['Rock','Paper','Scissors'])
-    
     print(f'Computer chose {computer_choice}')
 
-
     if user_choice == 'Rock' and computer_choice == 'Paper':
-        print('Computer Won')
+        message = 'Computer Won!'
     elif user_choice == 'Rock' and computer_choice == 'Scissors':
-        print('You Won')
+        message = 'You Won!'
     elif user_choice == 'None':
-        print('You Chose Nothing, You Lost')
+        message = 'You Chose Nothing, You Lost!'
     elif user_choice == 'Paper' and computer_choice == 'Rock':
-        print('You Won')
+        message = 'You Won!'
     elif user_choice == 'Paper' and computer_choice == 'Scissors':
-        print('You Lost')
-
+        message = 'You Lost!'
     elif user_choice == 'Scissors' and computer_choice == 'Rock':
-        print('You Lost')
-    elif user_choice == 'Scissors' and computer_choice == 'Paper':
-        print('You Won')
-    
+        message = 'You Lost!'
+    elif user_choice == 'Scissors!' and computer_choice == 'Paper':
+        message = 'You Won!'
     else:
-        print('Draw')
-
+        message = 'Draw'
+    cv2.putText(frame, message, (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-            
 # After the loop release the cap object
 cap.release()
 # Destroy all the windows
