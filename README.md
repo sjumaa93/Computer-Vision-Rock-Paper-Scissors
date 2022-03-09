e654c1cab00cb03ce5df3f68da0e2a975dfa562c
sjumaa93 <sjumaa93@gmail.com>
2022-03-09 21:05:26 +0000

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Computer-Vision-Rock-Paper-Scissors
+
+
+# How Does it Work?
+The model 'keras_model_h5' has been trained to detect whether I am showing rock, paper or scissors. The computer game then randomly generates rock, paper or scissors. And then a winner of that move is decided.
+
--- file.py
+++ file.py
@@ -2,10 +2,12 @@ import cv2
 from keras.models import load_model
 import numpy as np
 import random
+import time
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
+t_0 = time.time()
 while True: 
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
@@ -14,7 +16,6 @@ while True:
     data[0] = normalized_image
     prediction = model.predict(data)
 
-    # Press q to close the window
 
     if(prediction[0][3]) > 0.5:
         user_choice = ('None')
@@ -24,11 +25,16 @@ while True:
         user_choice = ('Scissors')
     else:
         user_choice = ('Rock')
+
     print(f'You chose {user_choice}')
-
+ 
+    time.sleep(3)
+ 
     computer_choice = random.choice(['Rock','Paper','Scissors'])
+   
     print(f'Computer chose {computer_choice}')
-
+   
+    time.sleep(3)
     if user_choice == 'Rock' and computer_choice == 'Paper':
         message = 'Computer Won!'
     elif user_choice == 'Rock' and computer_choice == 'Scissors':
@@ -45,6 +51,8 @@ while True:
         message = 'You Won!'
     else:
         message = 'Draw'
+    
+    time.sleep(3)
     cv2.putText(frame, message, (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
