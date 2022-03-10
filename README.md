6623f1752c2057574e22d49a4abd0bac4c5c1aae
sjumaa93 <sjumaa93@gmail.com>
2022-03-10 18:28:02 +0000

    created functions

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Computer-Vision-Rock-Paper-Scissors
 
-
-# How Does it Work?
 The model 'keras_model_h5' has been trained to detect whether I am showing rock, paper or scissors. The computer game then randomly generates rock, paper or scissors. And then a winner of that move is decided.
 
+A virtual enviroment was set up and installed with opencv, tensorflow and kernal. All of which are needed to run the program.
--- file.py
+++ file.py
@@ -6,35 +6,10 @@ import time
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-
 t_0 = time.time()
-while True: 
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    prediction = model.predict(data)
 
 
-    if(prediction[0][3]) > 0.5:
-        user_choice = ('None')
-    elif(prediction[0][1]) > 0.5:
-        user_choice = ('Paper')
-    elif(prediction[0][2]) > 0.5:
-        user_choice = ('Scissors')
-    else:
-        user_choice = ('Rock')
-
-    print(f'You chose {user_choice}')
- 
-    time.sleep(3)
- 
-    computer_choice = random.choice(['Rock','Paper','Scissors'])
-   
-    print(f'Computer chose {computer_choice}')
-   
-    time.sleep(3)
+def compare_options(computer_choice,user_choice):   
     if user_choice == 'Rock' and computer_choice == 'Paper':
         message = 'Computer Won!'
     elif user_choice == 'Rock' and computer_choice == 'Scissors':
@@ -51,8 +26,39 @@ while True:
         message = 'You Won!'
     else:
         message = 'Draw'
+    return message
+
+def get_user_choice(prediction):
+    if(prediction[0][3]) > 0.5:
+        user_choice = ('None')
+    elif(prediction[0][1]) > 0.5:
+        user_choice = ('Paper')
+    elif(prediction[0][2]) > 0.5:
+        user_choice = ('Scissors')
+    else:
+        user_choice = ('Rock')
     
-    time.sleep(3)
+    return user_choice
+
+
+while True: 
+    ret, frame = cap.read()
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+    data[0] = normalized_image
+    prediction = model.predict(data)
+    cv2. flip(frame,1)  #flip camera
+
+    user_choice = get_user_choice(prediction)
+    computer_choice = random.choice(['Rock','Paper','Scissors'])
+   
+    print(f'Computer chose {computer_choice}')
+    print(f'You chose {user_choice}')
+
+   
+    message = compare_options(computer_choice, user_choice)
+    
     cv2.putText(frame, message, (10, 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
